214386127fad7b5f6891f1b909c0f40a46f38f2b
jyz0309 <45495947@qq.com>
2025-04-22 16:00:01 +0800

    add types
    Signed-off-by: jyz0309 <45495947@qq.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/jyz0309/omcp-plugin
 
-go 1.22.2
+go 1.23
+
+toolchain go1.23.8
+
+require github.com/mark3labs/mcp-go v0.20.0
+
+require github.com/yosida95/uritemplate/v3 v3.0.2 // indirect
--- types/types.go
+++ types/types.go
@@ -1,8 +1,27 @@
 package types
 
+import (
+	"context"
+	"time"
+
+	"github.com/mark3labs/mcp-go/mcp"
+)
+
 type Plugin struct {
 	MCPType    string `json:"mcp_type"`
 	Name       string `json:"name"`
 	VarName    string `json:"var_name"`
 	PluginFile string `json:"plugin_file"`
+}
+
+// Tool is mcp tool for the MCP server,
+// it can be loadded as a plugin and add to server dynamically
+type MCPTool struct {
+	Name      string    `json:"name"`
+	Desc      string    `json:"desc"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+
+	Option  []mcp.ToolOption                                                                    `json:"-"`
+	Handler func(ctx context.Context, request mcp.CallToolRequest) (*mcp.CallToolResult, error) `json:"-"`
 }
